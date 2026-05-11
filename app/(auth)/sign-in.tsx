9f02b03c64763f48b827b28da732057c9b9dbb26
ojobointernational <91267354+ojobointernational@users.noreply.github.com>
2026-05-11 17:50:02 +0100

    Initial commit

---
+++ app/(auth)/sign-in.tsx
@@ -0,0 +1,14 @@
+import { Link } from "expo-router";
+import React from "react";
+import { Text, View } from "react-native";
+
+const SignIn = () => {
+  return (
+    <View>
+      <Text> Sign In</Text>
+      <Link href="/(auth)/sign-up">Create an Account</Link>
+    </View>
+  );
+};
+
+export default SignIn;
