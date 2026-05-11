bdddd49ae823af60ee62014db323c750c5740d07
ojobointernational <91267354+ojobointernational@users.noreply.github.com>
2026-05-11 19:16:21 +0100

    Added Tabs

--- app/(tabs)/subscription.tsx
+++ app/(tabs)/subscription.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Text, View } from "react-native";
 
-const subscriptions = () => {
+const subscription = () => {
   return (
     <View>
-      <Text>subscriptions</Text>
+      <Text>subscription</Text>
     </View>
   );
 };
 
-export default subscriptions;
+export default subscription;
